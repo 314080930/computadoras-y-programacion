11e72ad9f20607ffcf37b0a4650e21e3967f09ff
314080930 <31664360+314080930@users.noreply.github.com>
2017-10-31 10:20:17 -0600

    Add files via upload

---
+++ tarea 14/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define MAX 5
+
+int main(int argc, char *argv[]) {
+	int edad=0;
+	const float PI=3.1416;
+
+  printf("¿cual es tu edad?:");
+  scanf("%d ,&edad");
+  
+  printf("tu edad es=% \n",edad);
+  
+  printf("max=%d" ,MAX);
+  
+  printf("valor de PI=%f" ,PI);
+	return 0;
+}

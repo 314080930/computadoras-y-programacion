184917c32a9647517d8d7962b34ef75dc4dca54e
314080930 <31664360+314080930@users.noreply.github.com>
2017-11-14 13:10:20 -0600

    Add files via upload

---
+++ tarea 21/letras.h
@@ -0,0 +1,34 @@
+void letraa(){
+
+	printf("A A A A\nA     A  \nA  A  A\nA     A\nA     A\n\n");
+
+}
+void letran(){
+
+	printf("N     N\nN N   N\nN   N N\nN     N\nN     N\n\n");
+
+}
+
+
+
+void letrag(){
+
+    printf("G G G G\nG\nG G G G\nG     G\nG G G G\n\n"); 
+
+}
+
+
+
+void letrae(){
+
+	printf("E E E E\nE\nE E E\nE\nE E E E\n\n");
+
+}
+
+
+
+void letral(){
+
+	printf("L\nL\nL\nL\nL L L L\n\n");
+}
+
---
+++ tarea 21/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include "cabecera.h"
+
+#include "letras.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+
+int main(int argc, char *argv[]) {
+
+	
+
+	appInfoData("Tarea 21. Letras","13/11/2017");
+
+	
+
+	letraa();
+
+	letran();
+	
+	letrag();
+
+	letrae();
+
+	letral();
+
+	
+
+	system ("pause");
+
+	return 0;
+
+}
